983b406164c2499a4bb1e2242ff841874d6948b8
Junia <juniatech@gmail.com>
2022-05-11 17:34:31 -0300

    Contador e correções

---
+++ 02_Módulo_III_Javascript/Introducao_JavaScript/assets/js/scripts.js
@@ -0,0 +1,17 @@
+let count = 0;
+
+const CURRENT_NUMBER = document.getElementById('currentNumber');
+
+function increment() {
+	count++;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function decrement() {
+	count--;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function test() {
+	kdowkdpo;
+}
